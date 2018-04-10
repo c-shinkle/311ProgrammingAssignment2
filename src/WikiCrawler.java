8f8446c071b3dacbd8ac5970536200f03e19bf12
Christian <cshinkle@iastate.edu>
2018-04-10 16:52:07 -0500

    "Adding code from pdf"

--- src/WikiCrawler.java
+++ src/WikiCrawler.java
@@ -11,15 +11,23 @@
 * @author Christian Shinkle, Alec Harrison, Benjamin Trettin
 */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class WikiCrawler {
-	
+
 	public static final String BASE_URL = "https://en.wikipedia.org";
 
 	private int max;
 
-	private String url;
+	private String seedUrl;
 
 	private ArrayList<String> topics;
 
@@ -27,14 +35,26 @@ public class WikiCrawler {
 
 	public WikiCrawler(String seedUrl, int max, ArrayList<String> topics, String fileName) {
 		this.max = max;
-		this.url = seedUrl;
+		this.seedUrl = seedUrl;
 		this.topics = topics;
 		this.fileName = fileName;
 	}
 
-	public void crawl(){
-		
+	public void crawl() {
+		URL url = null;
+		InputStream is = null;
+		try {
+			url = new URL(BASE_URL + seedUrl);
+			is = url.openStream();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			return;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		Queue<String> queue = new LinkedList<String>();
+
 	}
-	
-	
 }
