79534ad095d8bde3fde37ffd3429248be2863212
Christian <cshinkle@iastate.edu>
2018-04-10 15:47:16 -0500

    Added stubs from TA's.

--- src/NetworkInfluence.java
+++ src/NetworkInfluence.java
@@ -1,62 +1,95 @@
+// LEAVE THIS FILE IN THE DEFAULT PACKAGE
+//  (i.e., DO NOT add 'package cs311.pa1;' or similar)
+
+// DO NOT MODIFY THE EXISTING METHOD SIGNATURES
+//  (you may, however, add member fields and additional methods)
+
+// DO NOT INCLUDE LIBRARIES OUTSIDE OF THE JAVA STANDARD LIBRARY
+//  (i.e., you may only include libraries of the form java.*)
+
+/**
+* @author Christian Shinkle, Alec Harrison, Benjamin Trettin
+*/
+
 import java.util.ArrayList;
 
-public class NetworkInfluence {
-	
-	/*/	graphData holds the absolute path of a file that stores a
-	directed graph. This file will be of the following format: First line indicates number of vertices.
-	Each subsequent line lists a directed edge of the graph. The vertices of this graph are represented
-	as strings. For example contents of the file may look like
-	/*/
-	NetworkInfluence(String graphData) {
-		
+public class NetworkInfluence
+{
+	// NOTE: graphData is an absolute file path that contains graph data, NOT the raw graph data itself
+	public NetworkInfluence(String graphData)
+	{
+		// implementation
 	}
-	
-	//Returns the out degree of v.
-	public int outDegree(String v){
-		return 0;
+
+	public int outDegree(String v)
+	{
+		// implementation
+
+		// replace this:
+		return -1;
 	}
-	
-	/*/Returns a BFS path from u to v. This method returns an array
-	list of strings that represents a shortest path from u to v. Note that this method must return an
-	array list of Strings. First vertex in the path must be u and the last vertex must be v. If there is no
-	path from u to v, then this method returns an empty list. The return type is ArrayList<String>
-	/*/
-	public ArrayList<String> shortestPath(String u, String v){
+
+	public ArrayList<String> shortestPath(String u, String v)
+	{
+		// implementation
+
+		// replace this:
 		return null;
 	}
 
-	//Returns dist(u, v). Type is int.
-	public int distance(String u, String v){
-		return 0;
+	public int distance(String u, String v)
+	{
+		// implementation:
+
+		// replace this:
+		return -1;
 	}
-	
-	//Here s a subset of vertices. This method returns dist(s, v). Type is int.
-	public int distance(ArrayList<String> s, String v){
-		return 0;
+
+	public int distance(ArrayList<String> s, String v)
+	{
+		// implementation
+
+		// replace this:
+		return -1;
 	}
-	
-	//Returns Inf(x). Return type is float.
-	public float influence(String u){
-		return 0;
+
+	public float influence(String u)
+	{
+		// implementation
+
+		// replace this:
+		return -1f;
 	}
-	
-	//Here s holds a set of vertices of G. This method returns Inf(s) whose type is float.
-	public float influence(ArrayList<String> s){
-		return 0;
+
+	public float influence(ArrayList<String> s)
+	{
+		// implementation
+
+		// replace this:
+		return -1f;
 	}
-	
-	//Returns a set of k nodes obtained by using Degree Greedy algorithm. Return type must be ArrayList<String>
-	public ArrayList<String> mostInfluentialDegree(int k){
+
+	public ArrayList<String> mostInfluentialDegree(int k)
+	{
+		// implementation
+
+		// replace this:
 		return null;
 	}
-	
-	//Returns a set of k nodes obtained by using Modular Greedy algorithm. Return type must be ArrayList<String>.
-	public ArrayList<String> mostInfluentialModular(int k){
+
+	public ArrayList<String> mostInfluentialModular(int k)
+	{
+		// implementation
+
+		// replace this:
 		return null;
 	}
-	
-	//Returns a set of k nodes obtained by using SubModular Greedy algorithm. Return type must be ArrayList<String>.
-	public ArrayList<String> mostInfluentialSubModular(int k){
+
+	public ArrayList<String> mostInfluentialSubModular(int k)
+	{
+		// implementation
+
+		// replace this:
 		return null;
 	}
 }
