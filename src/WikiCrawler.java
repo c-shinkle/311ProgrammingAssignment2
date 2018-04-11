4dccfb0c959d2e9ff10509fa9e01c7e8d4f2dca7
btrettin <batrettin@gmail.com>
2018-04-11 17:56:07 -0500

    implemented most of fetchPage and basic layout of crawl method

--- src/WikiCrawler.java
+++ src/WikiCrawler.java
@@ -33,6 +33,8 @@ public class WikiCrawler {
 
 	private String fileName;
 
+	private static int requests;
+
 	public WikiCrawler(String seedUrl, int max, ArrayList<String> topics, String fileName) {
 		this.max = max;
 		this.seedUrl = seedUrl;
@@ -40,21 +42,57 @@ public class WikiCrawler {
 		this.fileName = fileName;
 	}
 
-	public void crawl() {
+	public void crawl() throws IOException {
+		Queue<String> queue = new LinkedList<String>();
+		LinkedList<String> visited = new LinkedList<String>();
+		queue.add(seedUrl);
+		visited.add(seedUrl);
+		while (!queue.isEmpty()) {
+			String currentPage = queue.peek();
+			String currentPageHTML = fetchPage(currentPage);
+			ArrayList<String> currentPageLinks = extractLinks(currentPageHTML);
+			for (int i = 0; i < currentPageLinks.size(); i++) {
+				for (int k = 0; k < visited.size(); k++) {
+					if (!(visited.get(k).contains(currentPageLinks.get(i)))) {
+							queue.add(currentPageLinks.get(i));
+							visited.add(currentPageLinks.get(i));
+					}
+				}
+			}
+		}
+	}
+
+	private ArrayList<String> extractLinks(String currentPageHTML) {
+		ArrayList<String> currentPageLinks = new ArrayList<String>();
+		//TODO
+		return currentPageLinks;
+	}
+
+	private String fetchPage(String currentPage) throws IOException {
 		URL url = null;
 		InputStream is = null;
 		try {
-			url = new URL(BASE_URL + seedUrl);
+			url = new URL(BASE_URL + currentPage);
 			is = url.openStream();
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
-			return;
+			return null;
 		} catch (IOException e) {
 			e.printStackTrace();
-			return;
+			return null;
 		}
 		BufferedReader br = new BufferedReader(new InputStreamReader(is));
-		Queue<String> queue = new LinkedList<String>();
+		String inputLine;
+		StringBuilder a = new StringBuilder();
+		while ((inputLine = br.readLine()) != null) {
+			a.append(inputLine);
+		}
+		br.close();
+		return a.toString();
+	}
 
+	public static void main(String[] args) throws IOException {
+		WikiCrawler example = new WikiCrawler("/wiki/Complexity_theory", 20, null, "wikiCC.txt");
+		example.crawl();
 	}
 }
