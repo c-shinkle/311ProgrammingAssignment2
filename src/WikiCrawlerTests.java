dfbbe4dd1ba2658da423cf771ae37cedea8715ba
btrettin <batrettin@gmail.com>
2018-04-11 22:12:59 -0500

    added iowaState and complexityTheory web crawler tests

--- src/WikiCrawlerTests.java
+++ src/WikiCrawlerTests.java
@@ -2,25 +2,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
 class WikiCrawlerTests {
 
-	ArrayList<String> topics = new ArrayList<String>();
-	WikiCrawler complexityTheory = new WikiCrawler("/wiki/Complexity_theory", 20, topics, "wikiCC.txt");
+	ArrayList<String> complexTheorytopics = new ArrayList<String>();
+	ArrayList<String> ISUtopics = new ArrayList<String>(Arrays.asList("IowaState","cyclones"));
 	
+	WikiCrawler complexityTheory = new WikiCrawler("/wiki/Complexity_theory", 20, complexTheorytopics, "wikiCC.txt");
+	WikiCrawler iowaState = new WikiCrawler("/wiki/Iowa_State_University", 100, ISUtopics, "WikiISU.txt");
 	
 	@Test
 	void hasTopics1() throws IOException {
 		complexityTheory.crawl();
-		String expected = null;
+		ArrayList<String> expected = new ArrayList<String>(Arrays.asList("IowaState","cyclones"));;
 		String actual = null;
 		assertEquals(expected,actual);
 	}
 	@Test
 	void hasTopics2() throws IOException {
-		complexityTheory.crawl();
+		iowaState.crawl();
 		String expected = null;
 		String actual = null;
 		assertEquals(expected,actual);
@@ -34,7 +37,7 @@ class WikiCrawlerTests {
 	}
 	@Test
 	void extractLinks2() throws IOException {
-		complexityTheory.crawl();
+		iowaState.crawl();
 		String expected = null;
 		String actual = null;
 		assertEquals(expected,actual);
@@ -48,7 +51,7 @@ class WikiCrawlerTests {
 	}
 	@Test
 	void fetchPage2() throws IOException {
-		complexityTheory.crawl();
+		iowaState.crawl();
 		String expected = null;
 		String actual = null;
 		assertEquals(expected,actual);
@@ -69,14 +72,14 @@ class WikiCrawlerTests {
 	}
 	@Test
 	void Crawl3() throws IOException {
-		complexityTheory.crawl();
+		iowaState.crawl();
 		String expected = null;
 		String actual = null;
 		assertEquals(expected,actual);
 	}
 	@Test
 	void Crawl4() throws IOException {
-		complexityTheory.crawl();
+		iowaState.crawl();
 		String expected = null;
 		String actual = null;
 		assertEquals(expected,actual);
