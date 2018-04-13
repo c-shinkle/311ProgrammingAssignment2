076ae8fa4d21f3d44d32b77e5f103405f5076f73
btrettin <batrettin@gmail.com>
2018-04-12 23:49:36 -0500

    implemented constructor for NetWorkInfluence

--- src/NetworkInfluence.java
+++ src/NetworkInfluence.java
@@ -11,85 +11,111 @@
 * @author Christian Shinkle, Alec Harrison, Benjamin Trettin
 */
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
-public class NetworkInfluence
-{
-	// NOTE: graphData is an absolute file path that contains graph data, NOT the raw graph data itself
-	public NetworkInfluence(String graphData)
-	{
-		// implementation
+public class NetworkInfluence {
+
+	HashMap<String, ArrayList<String>> graph;
+
+	// NOTE: graphData is an absolute file path that contains graph data, NOT the
+	// raw graph data itself
+	public NetworkInfluence(String graphData) throws IOException {
+		this.graph = new HashMap<String, ArrayList<String>>();
+		File graphFile = new File(graphData);
+		String line;
+		BufferedReader reader = new BufferedReader(new FileReader(graphFile));
+		while ((line = reader.readLine()) != null) {
+			String[] parts = line.split(" ", 2);
+			String key = parts[0];
+			String value = parts[1];
+			if (graph == null || !graph.containsKey(key)) {
+				ArrayList<String> edges = new ArrayList<String>();
+				edges.add(value);
+				graph.put(key, edges);
+			}
+			else {
+				ArrayList<String> edges2 = graph.get(key);
+				if(!edges2.contains(value)) {
+					edges2.add(value);
+					graph.put(key, edges2);
+				}
+			}
+		}
+		reader.close();
 	}
 
-	public int outDegree(String v)
-	{
+	public int outDegree(String v) {
 		// implementation
 
 		// replace this:
 		return -1;
 	}
 
-	public ArrayList<String> shortestPath(String u, String v)
-	{
+	public ArrayList<String> shortestPath(String u, String v) {
 		// implementation
 
 		// replace this:
 		return null;
 	}
 
-	public int distance(String u, String v)
-	{
+	public int distance(String u, String v) {
 		// implementation:
 
 		// replace this:
 		return -1;
 	}
 
-	public int distance(ArrayList<String> s, String v)
-	{
+	public int distance(ArrayList<String> s, String v) {
 		// implementation
 
 		// replace this:
 		return -1;
 	}
 
-	public float influence(String u)
-	{
+	public float influence(String u) {
 		// implementation
 
 		// replace this:
 		return -1f;
 	}
 
-	public float influence(ArrayList<String> s)
-	{
+	public float influence(ArrayList<String> s) {
 		// implementation
 
 		// replace this:
 		return -1f;
 	}
 
-	public ArrayList<String> mostInfluentialDegree(int k)
-	{
+	public ArrayList<String> mostInfluentialDegree(int k) {
 		// implementation
 
 		// replace this:
 		return null;
 	}
 
-	public ArrayList<String> mostInfluentialModular(int k)
-	{
+	public ArrayList<String> mostInfluentialModular(int k) {
 		// implementation
 
 		// replace this:
 		return null;
 	}
 
-	public ArrayList<String> mostInfluentialSubModular(int k)
-	{
+	public ArrayList<String> mostInfluentialSubModular(int k) {
 		// implementation
 
 		// replace this:
 		return null;
+	}
+
+	public static void main(String[] args) throws IOException {
+		NetworkInfluence example2 = new NetworkInfluence("wikiCC.txt");
+		example2.outDegree(null);
 	}
 }
