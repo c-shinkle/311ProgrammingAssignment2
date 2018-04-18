b9f41c48534adb35964867813e298301f8c1a43d
Benjamin Trettin <ben@benmac.student.iastate.edu>
2018-04-17 19:28:26 -0500

    .

--- src/InfluenceTest.java
+++ src/InfluenceTest.java
@@ -22,4 +22,31 @@ public class InfluenceTest {
 		float actual = A.influence("A");
 		assertEquals(expected, actual,.001);
 	}
+	@Test
+	public void testOutDegree() {
+		int expected = 2;
+		int actual = A.outDegree("A");
+		assertEquals(expected,actual);
+	}
+	@Test
+	public void testMostInfluentialDegree() {
+		ArrayList<String> expected = new ArrayList<String>();
+		expected.add("A");
+		ArrayList<String> actual = A.mostInfluentialDegree(1);
+		assertEquals(expected,actual);
+	}
+	@Test
+	public void testMostInfluentialModular() {
+		ArrayList<String> expected = new ArrayList<String>();
+		expected.add("A");
+		expected.add("B");
+		ArrayList<String> actual = A.mostInfluentialModular(2);
+		assertEquals(expected,actual);
+	}
+	@Test
+	public void testMostInfluentialSubModular() {
+		ArrayList<String> expected = new ArrayList<String>();
+		ArrayList<String> actual = A.mostInfluentialSubModular(2);
+		assertEquals(expected,actual);
+	}
 }
